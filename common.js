4190805e0d790a46a32e11c1ba8a4c02d2599787
Amin Rahimi <a.rahimi@saadafzar.com>
2026-08-06 12:28:04 +0330

    refactor: getNearSettlementPrice

--- common.js
+++ common.js
@@ -464,29 +464,40 @@ export const getNearSettlementPrice = ({ strategyPosition, stockPrice, stockPric
 
   const tradeFee = strategyPosition.isBuy ? COMMISSION_FACTOR.OPTION.BUY : COMMISSION_FACTOR.OPTION.SELL;
   const exerciseFee = COMMISSION_FACTOR.OPTION.SETTLEMENT.EXERCISE_FEE
-  const tax = isTaxFree(strategyPosition) ? 0 : COMMISSION_FACTOR.OPTION.SETTLEMENT.SELL_TAX;
-  const discounter = optionPremium => strategyPosition.isBuy ? (optionPremium - 0.7) : (optionPremium + 0.7)
+  // const tax = isTaxFree(strategyPosition) ? 0 : COMMISSION_FACTOR.OPTION.SETTLEMENT.SELL_TAX;
 
+  const isBuy = strategyPosition.isBuy;
 
   function calculateCallPrice(stockPrice, strikePrice) {
-    if (stockPrice <= strikePrice) return 0
-
-    const adjustedStockPrice = stockPrice / stockPriceAdjustFactor;
+    
+    const adjustedStockPrice = isBuy ?  stockPrice / stockPriceAdjustFactor : stockPrice * stockPriceAdjustFactor;
+    if (adjustedStockPrice <= strikePrice) return 0
+    
     let optionPremium = (adjustedStockPrice - (strikePrice * (1 + exerciseFee))) / (1 + tradeFee);
 
-    return discounter(optionPremium)
+    return optionPremium;
   }
 
   function calculatePutPrice(stockPrice, strikePrice) {
-    if (stockPrice >= strikePrice) return 0
-    const adjustedStockPrice = stockPrice * stockPriceAdjustFactor;
+    
+    
+    const adjustedStockPrice = isBuy? stockPrice * stockPriceAdjustFactor : stockPrice / stockPriceAdjustFactor;
+    if (adjustedStockPrice >= strikePrice) return 0
+   
+  
     let optionPremium = (strikePrice * (1 - exerciseFee) - adjustedStockPrice) / (1 + tradeFee);
-    return discounter(optionPremium)
+    return optionPremium;
     //  return (strikePrice * (1 - tax - exerciseFee) - adjustedStockPrice) / (1 + tradeFee);
   }
 
-  const price = strategyPosition.isOption?  strategyPosition.isCall ? calculateCallPrice(stockPrice, strategyPosition.strikePrice) : calculatePutPrice(stockPrice, strategyPosition.strikePrice) : stockPrice;
-  return price > 0 ? price : 0
+  let price = strategyPosition.isOption?  strategyPosition.isCall ? calculateCallPrice(stockPrice, strategyPosition.strikePrice) : calculatePutPrice(stockPrice, strategyPosition.strikePrice) : stockPrice;
+
+
+  price = isBuy
+    ? Math.floor(price)
+    : Math.ceil(price);
+
+  return Math.max(price, 0);
 }
 
 
